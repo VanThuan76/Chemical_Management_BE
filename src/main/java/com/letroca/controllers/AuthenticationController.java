b11d16f0e9a2e14d4052f9b6800910581a328ce2
Gabriel Spacki <ifprgabriel@gmail.com>
2024-04-01 21:30:50 -0300

    initial commit

---
+++ src/main/java/com/letroca/controllers/AuthenticationController.java
@@ -0,0 +1,51 @@
+package com.letroca.controllers;
+
+import com.letroca.entities.users.dtos.AuthenticationDTO;
+import com.letroca.entities.users.dtos.LoginResponseDTO;
+import com.letroca.entities.users.dtos.RegisterDTO;
+import com.letroca.entities.users.User;
+import com.letroca.infra.security.TokenService;
+import com.letroca.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody AuthenticationDTO authenticationDTO) {
+        var credentials = new UsernamePasswordAuthenticationToken(authenticationDTO.email(), authenticationDTO.password());
+        var auth = this.authenticationManager.authenticate(credentials);
+
+        var token = tokenService.generateToken((User) auth.getPrincipal());
+
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody RegisterDTO data) {
+        if (this.userRepository.findByEmail(data.email()) != null) return ResponseEntity.badRequest().build();
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+        User user = new User(data.name(), data.email(), encryptedPassword, data.role());
+
+        this.userRepository.save(user);
+
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/test/java/com/letroca/LetrocaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letroca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetrocaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
